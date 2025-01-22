9a910c7ef9efb22d854102f4cd3de764aeae913c
Vinod Kumar <vinodrao835@gmail.com>
2025-01-22 16:32:39 +0530

    Created the new migration that add constraints in Airport Table. ``` npx sequelize migration:generate --name update-city-airport-association ```

---
+++ src/migrations/20250122085535-update-city-airport-association.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /*== Migration will directly gives you DB level Access.
+    Note: This are happens only at DB level, to set-up for javascript level, we need to set-up independently.
+    ==*/
+
+    await queryInterface.addConstraint("Airports", {
+      type: "FOREIGN KEY",
+      fields: ["cityId"],
+      name: "city_fk_constraint",
+      references: {
+        table: "Cities",
+        field: "id"
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint("Airports", "city_fk_constraint");
+  }
+};
